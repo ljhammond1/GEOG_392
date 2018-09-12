6e25effa8e2f5161a0fb4336cb7d150713fb4268
Lauren Hammond <ljhammond1996@ad.geos.tamu.edu>
2018-09-12 12:48:18 -0500

    part3 fix

--- Lab/Week02/Part3.py
+++ Lab/Week02/Part3.py
@@ -1,7 +1,13 @@
+# define part3 list and test by printing
 part3 = [146, 875, 911, 83, 81, 439, 44, 5, 46, 76, 61, 68, 1, 14, 38, 26, 21]
 print (part3) 
 
-def SumListEvens:
+# define SumListEvens
+def SumListEvens(part3):
     result3 = 0
     for i in part3:
-        if isEven = 0 % 2 == 0
+        if i % 2 == 0:
+            result3 = result3 + i
+    return result3
+
+print (SumListEvens(part3))
